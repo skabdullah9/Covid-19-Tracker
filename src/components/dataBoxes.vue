10ee73d8106fa9ab77827dd513aa8cbb5a963c93
skabdullah9 <skabdullah9941@gmail.com>
2021-07-28 21:33:17 +0530

    responsive bug

--- src/components/dataBoxes.vue
+++ src/components/dataBoxes.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="grid md:grid-cols-2 gap-4">
+    <main class="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div class="bg-indigo-200 rounded text-center shadow-md p-10">
             <h3 class="text-3xl font-bold text-blue-900 mb-3">Cases</h3>
             <div class="text-2xl mb-2">
